e76e6edead65bb2b275ef5c119258c6fa88a5d8c
Italo Silva <italo.silva.silvera@outlook.com>
2022-05-18 07:53:16 -0500

    chore(ui): Added PrimeVue UI library components to use.

--- src/main.js
+++ src/main.js
@@ -8,12 +8,28 @@ import PrimeVue from "primevue/config";
 import "primevue/resources/themes/md-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Button from "primevue/button";
+import Toolbar from "primevue/toolbar";
+import InputText from "primevue/inputtext";
+import Sidebar from "primevue/sidebar";
+import Row from "primevue/row";
 
 const app = createApp(App);
 
 app.use(router);
 
-//PrimeVue Configuration
+// PrimeVue Configuration
 app.use(PrimeVue, { ripple: true });
+
+// PrimeVue Components
+app.component("pv-data-table", DataTable);
+app.component("pv-column", Column);
+app.component("pv-button", Button);
+app.component("pv-toolbar", Toolbar);
+app.component("pv-input-text", InputText);
+app.component("pv-sidebar", Sidebar);
+app.component("pv-row", Row);
 
 app.mount("#app");
