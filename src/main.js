3d5f69016893ecf9f711175539b647e43cd6ff3c
Italo Silva <italo.silva.silvera@outlook.com>
2022-05-18 07:43:40 -0500

    chore(ui): Added PrimeVue UI Library with Material Design Theme.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
 
-const app = createApp(App)
 
-app.use(router)
+// PrimeVue Styles
+import "primevue/resources/themes/md-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(router);
+
+//PrimeVue Configuration
+app.use(PrimeVue, { ripple: true });
+
+app.mount("#app");
